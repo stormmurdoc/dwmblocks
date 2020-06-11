ba1f0229e11223efcf55f5dc91bc1bb6b45a743a
stormmurdoc <murdoc@storm-clan.de>
2020-06-11 23:05:25 +0200

    reformated

--- config.h
+++ config.h
@@ -1,26 +1,24 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11},*/
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
-	/* {"",	"crypto",	0,	13}, */
-	/* {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20}, */
-	/* {"",	"price btc Bitcoin 💰",				0,	21}, */
-	/* {"",	"price lbc \"LBRY Token\" 📚",			0,	22}, */
-	{"",	"torrent",	20,	7},
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	18}, */
-	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"weather",	18000,	5},
-	{"",	"mailbox",	180,	12},
-	/* {"",	"nettraf",	1,	16}, */
-	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	3},
-	{"",	"clock",	60,	1},
-	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+   {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+   {"",	"db_pacpackages.sh",	                0,	8},
+   {"",	"db_news.sh",		                    0,	6},
+   {"",	"db_bandwith3.sh",	                    2,	13},
+   {"",	"db_music.sh",	                        11,	11},
+   {"",	"db_wlan.sh",	                        1,	16},
+   {"",	"db_cpu.sh",		                    10,	18},
+   {"",	"db_battery.sh",	                    5,	3},
+   {"",	"db_clock.sh",	                        60,	1},
+   {"",	"db_volume.sh",	                        0,	10},
+   /*
+   {"",	"moonphase",	18000,	17},
+   {"",	"memory",	10,	14},
+   {"",	"internet",	5,	4},
+   {"",	"help-icon",	0,	15},
+   {"",	"weather",	18000,	5},
+   {"",	"mailbox",	180,	12},
+   */
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
